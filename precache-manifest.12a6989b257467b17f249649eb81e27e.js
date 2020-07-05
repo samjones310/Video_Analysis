339e20ee936d994488ecc4f520f50983a9680d33
samjones310 <samjones31098@gmail.com>
2020-07-05 21:19:47 +0530

    Updates

---
+++ precache-manifest.12a6989b257467b17f249649eb81e27e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4e72a866fc448a1a0715c8ad01d1e3",
+    "url": "/Video_Analysis/index.html"
+  },
+  {
+    "revision": "2738b392795e1a74f673",
+    "url": "/Video_Analysis/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "78056fc51ed0846a538e",
+    "url": "/Video_Analysis/static/css/main.67791091.chunk.css"
+  },
+  {
+    "revision": "2738b392795e1a74f673",
+    "url": "/Video_Analysis/static/js/2.ebfa64ad.chunk.js"
+  },
+  {
+    "revision": "d4b07a3b200e54b60ce30bbb53d0a327",
+    "url": "/Video_Analysis/static/js/2.ebfa64ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78056fc51ed0846a538e",
+    "url": "/Video_Analysis/static/js/main.605d7842.chunk.js"
+  },
+  {
+    "revision": "9615468609a12680fc32",
+    "url": "/Video_Analysis/static/js/runtime-main.47ccc131.js"
+  },
+  {
+    "revision": "f69c58df1e9db99664837711b57ec128",
+    "url": "/Video_Analysis/static/media/profile.f69c58df.png"
+  }
+]);
